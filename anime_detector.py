f686c94954873c4a64202abcf1589e944ea8dd3c
Parkman145 <parkerlearmonth@gmail.com>
2025-07-02 20:14:10 -0700

    Update anime_detector

--- anime_detector.py
+++ anime_detector.py
@@ -1,16 +1,19 @@
 import asyncio
-import pickledb
 from google.cloud import vision
 from google.cloud.vision_v1 import ImageAnnotatorClient
 
-db = pickledb.load("pfp.db", True)
+db = {}
 
 
-def check_image_url(image):
+
+def check_image(url):
+
+    if url in db:
+        return db[url]
 
     client = ImageAnnotatorClient()
     response = client.annotate_image({
-        'image': {'source': {'image_uri': image}},
+        'image': {'source': {'image_uri': url}},
         'features': [
             {
                 'type_': vision.Feature.Type.LABEL_DETECTION, 
@@ -24,17 +27,10 @@ def check_image_url(image):
         if item.description == "Anime":
             has_anime_pfp = True
             break
+    db[url] = has_anime_pfp
     return has_anime_pfp
 
 
-
-
-
-
-def check_asset(asset):
-    if db.exists(asset.key):
-        return db.get(asset.key)
-    else:
-        isAnime = check_image_url(asset.url)
-        db.set(asset.key, isAnime)
-        return isAnime
+if __name__ == "__main__":
+    check_image("https://cdn.discordapp.com/attachments/755649995021090900/1389870798168264774/elgatobalatro_1.png?ex=6866da4c&is=686588cc&hm=7ef48e0da3b6b001ed3f8ecbaf79e99803a35166c0e0545271c1ad40a9d6169a&")
+    check_image("https://cdn.discordapp.com/attachments/755649995021090900/1389870798168264774/elgatobalatro_1.png?ex=6866da4c&is=686588cc&hm=7ef48e0da3b6b001ed3f8ecbaf79e99803a35166c0e0545271c1ad40a9d6169a&")
