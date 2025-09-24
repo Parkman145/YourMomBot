53cdc448f3df7272e9770a90bb0c83013daed44b
Parkman145 <ParkerLearmonth@gmail.com>
2025-09-23 18:15:20 -0700

    Implement has_nootice

--- Functions.py
+++ Functions.py
@@ -8,3 +8,18 @@ def random_chance(chance: float) -> bool:
 def getRandomTenor(token, query):
     r = requests.get(f"https://tenor.googleapis.com/v2/search?q={query}&key={token}&client_key=my_client_key&limit=1&random=true")
     return loads(r.content)["results"][0]["media_formats"]["gif"]["url"]
+    
+def has_nootice(string: str):
+    string = string.upper()
+    base = "NOTIC"
+    curr = 0
+    for char in string:
+        if curr == 2 and char == "O":
+            continue
+        if char != base[curr]:
+            curr = 0
+        if char == base[curr]:
+            if curr == 4:
+                return True
+            curr += 1
+    return False
